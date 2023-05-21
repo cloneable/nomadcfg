25949a3cebc436f8d15374e57ac384d20ce7854c
Folke Behrens <folke@gmail.com>
2023-05-21 20:21:52 +0200

    Support json and yaml output, imagetag as extvar

--- nomadcfg/src/main.rs
+++ nomadcfg/src/main.rs
@@ -1,14 +1,27 @@
 use clap::Parser;
 use jrsonnet_evaluator::{trace::PathResolver, FileImportResolver, State};
 use jrsonnet_stdlib::ContextInitializer;
+use serde::{Deserialize, Serialize};
 use std::error::Error;
 use std::path::PathBuf;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Opts {
-    #[arg(short, long, value_name = "FILE")]
+    #[arg(long, value_name = "FILE")]
     spec: PathBuf,
+
+    #[arg(long, default_value = "latest", value_name = "TAG")]
+    imagetag: String,
+
+    #[arg(long)]
+    yaml: bool,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+struct Jobspec {
+    #[serde(rename = "Job")]
+    job: nomadapi::types::Job,
 }
 
 pub fn main() -> Result<(), Box<dyn Error + 'static>> {
@@ -18,7 +31,7 @@ pub fn main() -> Result<(), Box<dyn Error + 'static>> {
     state.set_import_resolver(FileImportResolver::default());
 
     let ctx = ContextInitializer::new(state.clone(), PathResolver::new_cwd_fallback());
-    // ctx.add_ext_str("bar".into(), "bar-value".into());
+    ctx.add_ext_str("imagetag".into(), opts.imagetag.into());
     state.set_context_initializer(ctx);
 
     // let mut tla = GcHashMap::<IStr, IStr>::new();
@@ -27,9 +40,13 @@ pub fn main() -> Result<(), Box<dyn Error + 'static>> {
     let val = state.import(opts.spec)?;
     // let val = apply_tla(state.clone(), &tla, val)?;
 
-    let spec: nomadapi::types::Job = serde_jrsonnet::from_val(&val)?;
+    let spec: Jobspec = serde_jrsonnet::from_val(&val)?;
 
-    let output = serde_yaml::to_string(&spec)?;
+    let output = if opts.yaml {
+        serde_yaml::to_string(&spec)?
+    } else {
+        serde_json::to_string_pretty(&spec)?
+    };
 
     println!("{output}");
 
